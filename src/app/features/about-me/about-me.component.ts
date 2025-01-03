89ef7b9aaddf5cf72f6c73efe3422af7a3f05e0b
Carlos Rios <Carlos.Rios@ipsos.com>
2025-01-02 19:45:11 -0500

    Add banner component to About Me section and update header styles

--- src/app/features/about-me/about-me.component.ts
+++ src/app/features/about-me/about-me.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from './components/banner/banner.component';
 
 @Component({
   selector: 'app-about-me',
-  imports: [],
+  imports: [BannerComponent],
   templateUrl: './about-me.component.html',
   styleUrl: './about-me.component.scss'
 })
