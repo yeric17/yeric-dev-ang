df0032268cdda99dc4f8dc4030de781ce8113c50
Carlos Yeric Fonseca Rios <yeric17@gmail.com>
2025-01-06 19:50:50 -0500

    Add 'About Me' section and update experience descriptions; enhance navbar with new fragment link

--- src/app/features/about-me/data/experience.data.ts
+++ src/app/features/about-me/data/experience.data.ts
@@ -4,13 +4,13 @@ export const experiences: TimeLineInfo[] = [
     {
         title: 'Software Developer',
         subtitle: 'Ipsos',
-        description: 'Ipsos is a global market research and consulting firm. I am working as a software developer in the LATAM team.',
+        description: 'Developed web applications for data visualization, analysis, and process optimization using Angular, .NET, and SQL Server as part of the LATAM team. Also developed new AI-driven applications that not only optimized processes but also reduced costs, resulting in financial returns for the company.',
         info: 'November 2022 - Present'
     },
     {
         title: 'Scripting and Data Processing Assistant',
         subtitle: 'Ipsos',
-        description: 'Development of scripts using Javascript and Visual Basic, to process interviews for opinion studies mainly, created on the company\'s own platforms. I managed to implement templates for Online project work, which at the time did not exist within the area and became an option to hire new projects.',
+        description: 'Developed scripts using JavaScript and Visual Basic to process interviews for opinion studies, primarily created on the company\'s own platforms. Implemented templates for online project work, which were not available in the department at the time and became an option for new project acquisitions.',
         info: 'February 2017 - November 2022'
     }
 ];
--- src/app/features/about-me/data/skills.data.ts
+++ src/app/features/about-me/data/skills.data.ts
@@ -75,12 +75,12 @@ export const skills: Skill[] = [
         type: 'database',
         color: '#00758f',
     },
-    {
-        name: 'Docker',
-        image: 'docker.webp',
-        type: 'devops',
-        color: '#0db7ed',
-    },
+    // {
+    //     name: 'Docker',
+    //     image: 'docker.webp',
+    //     type: 'devops',
+    //     color: '#0db7ed',
+    // },
     {
         name: 'Github Actions',
         image: 'github-actions.png',
@@ -101,3 +101,4 @@ export const skills: Skill[] = [
         color: '#f57f17',
     }
 ]
+
